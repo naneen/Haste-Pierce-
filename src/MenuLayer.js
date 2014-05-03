24ddaaa65862328138b8298b2cce792a0b08daa8
naneen <neen_tta@hotmail.com>
2014-05-03 21:39:48 +0700

    เท่าเดิม

--- src/MenuLayer.js
+++ src/MenuLayer.js
@@ -19,6 +19,7 @@ var MenuLayer = cc.Layer.extend({
 	},
 
 	onTouchBegan:function( touch, event ) {
+        cc.log("==onplay clicked");
         var director = cc.Director.getInstance();
         director.replaceScene(cc.TransitionFade.create(1.5, new StartScene()));
     }
